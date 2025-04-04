008c6363b09db19aadc8416a80e6895e849ba84c
Stella <stella6319618@gmail.com>
2025-04-04 22:01:03 +0800

    fix: updated google-github-actions auth

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const mongooseOptions = {
 //   console.log("MongoDB disconnected");
 // });
 
-// Routes 
+// Routes 123
 const tripRoutes = require("./routes/trips");
 const userRoutes = require("./routes/users");
 
